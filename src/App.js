b33420b7322923b999bdf25797205252a4e297c3
Anmol Agrawal <anmol2002.aa1@gmail.com>
2025-07-04 01:59:28 +0530

    fix

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn Nodejssss
+          Learn No
         </a>
       </header>
     </div>
