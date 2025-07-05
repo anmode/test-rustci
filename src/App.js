ebd02d58a3bce0725b0b4f5b6baea80d6de10bd6
Anmol Agrawal <anmol2002.aa1@gmail.com>
2025-07-05 16:55:58 +0530

    added two job

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn repos
+          Learn reposi
         </a>
       </header>
     </div>
